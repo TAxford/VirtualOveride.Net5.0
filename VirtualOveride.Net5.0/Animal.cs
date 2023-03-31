c09a3a0f014ddcd782fd3db844fd571040578fad
Timothy Axford <t.axford2@yahoo.com>
2023-03-31 10:14:58 -0400

    Created an Animal class with properties Name, Age, IsHungry

---
+++ VirtualOveride.Net5.0/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOveride.Net5._0
+{
+    internal class Animal
+    {
+        //name properties        
+        public string Name { get; set; }
+        //age property
+        public int Age { get; set; }
+        //is hungry boolean to check wether the animal is hungry
+        public bool IsHungry { get; set; }
+    }
+}
