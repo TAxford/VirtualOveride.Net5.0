d1d8b9f7d9325ea885e788adbfc31cbbc139e36d
Timothy Axford <t.axford2@yahoo.com>
2023-03-31 10:30:18 -0400

    Added a simple contructor where we pass the name and age to our base constructor as well as added an addition property for IsHappy to the Dog Class

--- VirtualOveride.Net5.0/Dog.cs
+++ VirtualOveride.Net5.0/Dog.cs
@@ -8,6 +8,14 @@ namespace VirtualOveride.Net5._0
 {
     internal class Dog : Animal
     {
+        //bool property to check if dog is Happy
+        public bool IsHappy { get; set; }
 
+        //simple contructor where we pass the name and age to our base constructor
+        public Dog(string name, int age) : base(name, age) 
+        {
+            //all dogs are happy
+            IsHappy = true;
+        }
     }
 }
